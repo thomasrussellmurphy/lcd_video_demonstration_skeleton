f809a4ccd2807c6b5bc2b3bec7e2948a8f47d420
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-03-12 13:23:22 -0400

    Initial Commit
    Using single-project Quartus II .gitignore
    
    Complete project for testing out implementation of video_position_sync.

---
+++ V/video_position_sync.v
@@ -0,0 +1,13 @@
+module video_position_sync
+       (
+           input disp_clk,
+           input en,
+           output reg disp_hsync, disp_vsync,
+           output reg valid_draw, v_blank,
+           output reg [ 9: 0 ] h_pos, v_pos
+       );
+// Module header template
+// Replace with implemented version to test functionality
+
+
+endmodule
---
+++ lcd_demo.v
@@ -0,0 +1,90 @@
+module lcd_demo
+       (
+           //////////////////////// Clock Input ////////////////////////
+           input CLOCK_50,
+           input CLOCK_50_2,
+           //////////////////////// Push Button ////////////////////////
+           input [ 2: 0 ] BUTTON,
+           //////////////////////// DPDT Switch ////////////////////////
+           input [ 9: 0 ] SW,
+           //////////////////////// 7-SEG Display ////////////////////////
+           output [ 6: 0 ] HEX0_D,
+           output HEX0_DP,
+           output [ 6: 0 ] HEX1_D,
+           output HEX1_DP,
+           output [ 6: 0 ] HEX2_D,
+           output HEX2_DP,
+           output [ 6: 0 ] HEX3_D,
+           output HEX3_DP,
+           //////////////////////// LED ////////////////////////
+           output [ 9: 0 ] LEDG,
+           //////////////////////// GPIO ////////////////////////
+           input [ 1: 0 ] GPIO0_CLKIN,
+           output [ 1: 0 ] GPIO0_CLKOUT,
+           inout [ 31: 0 ] GPIO0_D,
+           input [ 1: 0 ] GPIO1_CLKIN,
+           output [ 1: 0 ] GPIO1_CLKOUT,
+           inout [ 31: 0 ] GPIO1_D
+       );
+
+// =======================================================
+// REG/WIRE declarations
+// =======================================================
+wire pll_lock;
+
+wire [ 9: 0 ] h_pos, v_pos;
+wire valid_draw, v_blank;
+
+wire en_switch;
+
+wire [ 7: 0 ] disp_red, disp_green, disp_blue;
+wire disp_clk, disp_en, disp_vsync, disp_hsync;
+
+//=======================================================
+// Input/Output assignments
+//=======================================================
+// All inout port turn to tri-state
+assign GPIO0_D = 32'hzzzzzzzz;
+assign GPIO1_D [ 31: 28 ] = 4'hz;
+
+// Hello!! BIG HINT HERE! (It'll make your life easier)
+assign GPIO1_D[ 27: 0 ] = { disp_vsync, disp_hsync, disp_en, disp_clk, disp_blue, disp_green, disp_red };
+assign disp_en = pll_lock; // Enable the display just after PLL has locked
+
+assign en_switch = SW[ 0 ];
+
+// =======================================================
+// Structural coding
+// =======================================================
+
+video_pll pll(
+              .inclk0( CLOCK_50 ),
+              .c0( disp_clk ),
+              .locked( pll_lock )
+          );
+
+// Control the video side of the world
+video_position_sync video_sync (
+                        .disp_clk( disp_clk ),
+                        .en( pll_lock ),
+                        .valid_draw( valid_draw ),
+                        .v_blank( v_blank ),
+                        .h_pos( h_pos ),
+                        .v_pos( v_pos ),
+                        .disp_hsync( disp_hsync ),
+                        .disp_vsync( disp_vsync )
+                    );
+
+demo_video video_gen
+           (
+               .clk( disp_clk ),
+               .en( en_switch ),
+               .x_pos( h_pos ),
+               .y_pos( v_pos ),
+               .valid_region( valid_draw ),
+               .v_blank( v_blank ),
+               .value_red( disp_red ),
+               .value_green( disp_green ),
+               .value_blue( disp_blue )
+           );
+endmodule
